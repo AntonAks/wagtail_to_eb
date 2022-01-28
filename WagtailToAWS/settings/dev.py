6d19bb9fa7a5e22ff7cc846cc866ec50e3d34d19
Anton Aksyonov <anton.aksyonov@intellecteu.com>
2022-01-28 15:38:45 +0200

    first commit

---
+++ WagtailToAWS/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-%zniwi43trq2bf#vbt!fb0x5r1yu04rn7xktej=-9+&w77^z0n'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
